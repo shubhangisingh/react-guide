d74686784362febcaee8b889cb43b4f3c8fb861d
shubhangi Singh <shubhangi.singh@leaseplan.com>
2020-03-29 23:45:52 +0530

    adding styling to components

--- src/App.js
+++ src/App.js
@@ -39,11 +39,22 @@ class App extends Component {
   })
   }
   render() {
+    const mystyle= {
+     backgroundColor: 'white',
+     font: 'inherit',
+     border: '1x solid blue',
+     padding: '8px',
+     cursor: 'pointer'
+
+    };
+
     return (
      <div className="App">
        <h1>{this.state.title}</h1>
        <p>This is working!!!</p>
-       <button onClick= { () => this.switchNameHandler('pradeep')}>Switch Name </button>
+       <button 
+        style= {mystyle}
+        onClick= { () => this.switchNameHandler('pradeep')}>Switch Name </button>
        <Person 
          name={this.state.persons[0].name} age= {this.state.persons[0].age} />
         <Person 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import './Person.css'
 const person = (props) => {
 	return (
-		<div onClick={props.click}>
+		<div onClick={props.click} className='Person'>
 		      <p > I'm {props.name} and i am {props.age} years old!!</p>
 		      <p>{props.children} </p>
 		      <input type='text' onChange={props.changed} value= {props.name} />
