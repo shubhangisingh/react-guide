b14bb3098bc9883e74a6cac560bc55842aefaf2c
shubhangi Singh <shubhangi.singh@leaseplan.com>
2020-04-23 22:51:06 +0530

    flexible lists

--- src/App.js
+++ src/App.js
@@ -18,15 +18,21 @@ class App extends Component {
 
 
 
-  nameChangedHandler =(event) => {
+  nameChangedHandler =(event, id) => {
+   const personIndex = this.state.persons.findIndex(p =>{
+    return p.id === id;
+   })
 
-     this.setState({
-       persons: [
-      {name: 'Max' ,age: '19'},
-      {name: 'Manushi', age: '9'},
-      {name: event.target.value, age: '13'}
-      ]
-  })
+   const person = {
+     ...this.state.persons[personIndex]}
+
+   person.name = event.target.value
+
+   const persons =[...this.state.persons];
+   
+   persons[personIndex] = person
+
+     this.setState({persons: persons  })
   }
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
@@ -63,7 +69,8 @@ class App extends Component {
                   click= {() => this.deletePersonHandler(index)}
                   name= {person.name}
                   age= {person.age}
-                  key= {person.id}/>
+                  key= {person.id}
+                  changed= {(event)=> this.nameChangedHandler(event, person.id)}/>
                 })}
               
           </div>  
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,9 +2,10 @@ import React from 'react'
 import './Person.css'
 const person = (props) => {
 	return (
-		<div onClick={props.click} className='Person'>
+		<div className='Person'>
 		      <p > I'm {props.name} and i am {props.age} years old!!</p>
 		      <p>{props.children} </p>
+		      <button onClick={props.click} > Delete</button>
 		      <input type='text' onChange={props.changed} value= {props.name} />
 		</div>
 		)
